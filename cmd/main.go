df7c0385b18049fadc8ad333e41395be46f80e77
raafly <rafliexecutor375@gmail.com>
2023-11-01 16:47:15 +0700

    error headling

--- cmd/main.go
+++ cmd/main.go
@@ -4,8 +4,8 @@ import (
 	"net/http"
 
 	"github.com/go-playground/validator/v10"
-	"github.com/raafly/food-app/pkg/configs"
 	"github.com/raafly/food-app/internal/listing"
+	"github.com/raafly/food-app/pkg/configs"
 	"github.com/raafly/food-app/pkg/route"
 )
  
@@ -21,7 +21,7 @@ func main() {
 	productSer := listing.NewProductService(productRepo, db, validate)
 	productHandler := listing.NewProductHandler(productSer)
 	route := route.NewRoute(customerHandler, productHandler)
-
+	
 	server := http.Server {
 		Handler: route,
 		Addr: ":3000",
